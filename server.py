a11f789a3d40b19de2029a3cde4c26b3f64a28fd
Franco <francobelman@live.com>
2021-10-08 17:46:55 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,113 @@
-from socket import * 
-serverPort = 12000 
-messageNumber = 1
+from _thread import start_new_thread
+import threading, socket, os
 
-serverSocket = socket(AF_INET,SOCK_STREAM) 
+# Static Variables
+HOST = '10.0.0.1'
+PORT = 65432
 
-serverSocket.bind(('',serverPort)) 
-serverSocket.listen(2) 
-print ('The server waiting to receive two connections...') 
-connectionSocket, addr = serverSocket.accept() 
-print('Accepted first connection, calling it client X') 
-connectionSocket2, addr = serverSocket.accept()
-print('Accepted second connection, calling it client Y')  
-connectionSocket.send(b'From Server: Client X Connected') 
-connectionSocket2.send(b'From Server: Client Y Connected')
+# Global variables
+messages = []
+clients = []
+msg_count = 0
+clients_lock = threading.Lock()
 
-print('Waiting to receive messages from client X and client Y...')   
-while messageNumber < 3:
-  messageOne = connectionSocket.recv(1024).decode()
-  messageTwo = connectionSocket2.recv(1024).decode()
-  if messageNumber == 1:
-    print('Client X sent message 1:', messageOne)        
-    messageNumber += 1
-    connectionSocket.close()  
-  if messageNumber == 2:
-    print('Client Y sent message 2:', messageTwo)  
-    messageNumber += 1
-    connectionSocket2.close() 
-serverSocket.close()
+def main():
+    global thread_count
+
+    # Open a socket for TCP usage.
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
+        # Listen for connections.
+        sock.bind((HOST, PORT))
+        sock.listen(2)
+
+        print('The server is waiting to receive 2 connections...')
+
+        try:
+            # Accept 2 client connections to the server and start a thread
+            # for each one of the connecting clients.
+            while True:
+                conn1, add = sock.accept()
+                print('Accepted 1st connection, calling it client X')
+
+                conn2, add = sock.accept()
+                print('Accepted 2nd connection, calling it client Y')
+
+                start_new_thread(threaded_client, (conn1, 'X'))
+                start_new_thread(threaded_client, (conn2, 'Y'))
+
+                print('\nWaiting to receive messages from client X and client Y...\n')
+        except:
+            print('exit')
+    
+def threaded_client(conn, client_name):
+    global msg_count
+    global messages
+    global clients
+
+    # Add client to a list so we can 
+    # access them whenenever we need to.
+    clients.append(conn)
+
+    # First message is sent to the client stating their client name.
+    connect_message = 'Client ' +  client_name + ' connected\n'
+    conn.sendall(connect_message.encode())
+
+    while True:
+
+        # Receive data from the clients.
+        try:
+            recv_data = conn.recv(1024).decode()
+        except:
+            print("Connections Closed, Exiting!")
+            exit()
+
+        # If the server sent something.
+        if not recv_data:
+            break       
+        else:
+            # Increase the message counter.
+            msg_count += 1
+
+            # Print the message recived by the server that was sent by the client.
+            message = 'Client ' + client_name + ' sent message ' + str(msg_count) + ': ' + recv_data + '\n'
+            print(message[:-1])
+
+            # Add messgae to a list of messages.
+            messages.append((client_name, recv_data))
+
+            with clients_lock:
+                try:
+                    # If the message list has 2 entries then we send a specific message to the
+                    # clients stating which one sent the first message and which one sent the 
+                    # second message.
+                    if len(messages) == 2:
+                        for c in clients:
+                            response = (messages[0][0] + ': ' + messages[0][1] + ' received before ' + 
+                                        messages[1][0] + ': ' + messages[1][1] + '\n')
+                            c.sendall(response.encode())
+                    
+                    # If the message recived by the server is "Bye" we send a Bye
+                    # to both servers telling them both to close. Then the connection
+                    # is closed within this thread and the program is closed. 
+                    if recv_data == "Bye":
+
+                        for c in clients:
+                            c.sendall(recv_data.encode())
+
+                        print("Terminating chat.")
+                        conn.close()
+                        os._exit(1)
+
+                    # If there's more than 2 messages in the list then we start sending 
+                    # all of the incoming  messages back to both clients. This allows them
+                    # to keep an accurate history of what's they're both sending and receiving.
+                    if len(messages) > 2:
+                        for c in clients:
+                            c.sendall(message.encode())
+                except:
+                    print('Tried to receive message after one client had been closed.')
+
+# Start program.
+if __name__ == '__main__':
+    main()
